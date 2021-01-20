b379a3902e3b8e4e96df9bc50ce7948d4716d132
BlackOfWorld <16164933+BlackOfWorld@users.noreply.github.com>
2021-01-20 13:26:35 +0100

    CI test.... again....

--- Build/build.bat
+++ Build/build.bat
@@ -2,21 +2,81 @@
 
 mkdir Build
 del /q /f /s .\Build
-dotnet msbuild ..\GladosV3.sln /p:Configuration=Release "/p:Platform=Any CPU"
-dotnet publish ..\GLaDOSV3\GladosV3.csproj -c Release --self-contained -r win-x64 -o .\Build\
-mkdir .\Build\Binaries\
-mkdir .\Build\Modules\
-mkdir .\Build\Images\
-mkdir .\Build\Dependencies\
-
-copy ..\GLaDOSV3\bin\Release\netcoreapp3.0\Binaries\ .\Build\Binaries\
-copy ..\GLaDOSV3\bin\Release\netcoreapp3.0\Modules\ .\Build\Modules\
-copy ..\GLaDOSV3\bin\Release\netcoreapp3.0\Images\ .\Build\Images\
-copy ..\GLaDOSV3\bin\Release\netcoreapp3.0\Dependencies\ .\Build\Dependencies\
+dotnet msbuild ..\GladosV3.sln /p:Configuration=Release "/p:Platform=x64"
+dotnet publish ..\GLaDOSV3\GladosV3.csproj -c Release -p:PublishSingleFile=true --self-contained true -r win-x64 -o .\Build\win-x64
 IF %ERRORLEVEL% NEQ 0 GOTO err
-del release.zip
-7z a release.zip -mx9 -r .\Build\* .\Build\*
-7z l release.zip
+set BUILDLOCATION=.\Build\win-x64
+mkdir %BUILDLOCATION%\Binaries\
+mkdir %BUILDLOCATION%\Modules\
+mkdir %BUILDLOCATION%\Images\
+mkdir %BUILDLOCATION%\Dependencies\
+copy ..\GLaDOSV3\bin\Release\net5.0\Binaries\     %BUILDLOCATION%\Binaries\
+copy ..\GLaDOSV3\bin\Release\net5.0\Modules\      %BUILDLOCATION%\Modules\
+copy ..\GLaDOSV3\bin\Release\net5.0\Images\       %BUILDLOCATION%\Images\
+copy ..\GLaDOSV3\bin\Release\net5.0\Dependencies\ %BUILDLOCATION%\Dependencies\
+
+dotnet msbuild ..\GladosV3.sln /p:Configuration=Release "/p:Platform=x86"
+dotnet publish ..\GLaDOSV3\GladosV3.csproj -c Release -p:PublishSingleFile=true --self-contained true -r win-x86 -o .\Build\win-x86
+IF %ERRORLEVEL% NEQ 0 GOTO err
+set BUILDLOCATION=.\Build\win-x86
+mkdir %BUILDLOCATION%\Binaries\
+mkdir %BUILDLOCATION%\Modules\
+mkdir %BUILDLOCATION%\Images\
+mkdir %BUILDLOCATION%\Dependencies\
+copy ..\GLaDOSV3\bin\Release\net5.0\Binaries\     %BUILDLOCATION%\Binaries\
+copy ..\GLaDOSV3\bin\Release\net5.0\Modules\      %BUILDLOCATION%\Modules\
+copy ..\GLaDOSV3\bin\Release\net5.0\Images\       %BUILDLOCATION%\Images\
+copy ..\GLaDOSV3\bin\Release\net5.0\Dependencies\ %BUILDLOCATION%\Dependencies\
+
+dotnet msbuild ..\GladosV3.sln /p:Configuration=Release "/p:Platform=x64"
+dotnet publish ..\GLaDOSV3\GladosV3.csproj -c Release -p:PublishSingleFile=true --self-contained true -r linux-x64 -o .\Build\linux-x64
+IF %ERRORLEVEL% NEQ 0 GOTO err
+set BUILDLOCATION=.\Build\linux-x64
+mkdir %BUILDLOCATION%\Binaries\
+mkdir %BUILDLOCATION%\Modules\
+mkdir %BUILDLOCATION%\Images\
+mkdir %BUILDLOCATION%\Dependencies\
+copy ..\GLaDOSV3\bin\Release\net5.0\Binaries\     %BUILDLOCATION%\Binaries\
+copy ..\GLaDOSV3\bin\Release\net5.0\Modules\      %BUILDLOCATION%\Modules\
+copy ..\GLaDOSV3\bin\Release\net5.0\Images\       %BUILDLOCATION%\Images\
+copy ..\GLaDOSV3\bin\Release\net5.0\Dependencies\ %BUILDLOCATION%\Dependencies\
+IF %ERRORLEVEL% NEQ 0 GOTO err
+dotnet msbuild ..\GladosV3.sln /p:Configuration=Release "/p:Platform=x64"
+dotnet publish ..\GLaDOSV3\GladosV3.csproj -c Release -r portable -o .\Build\portable-x64
+
+set BUILDLOCATION=.\Build\portable-x64
+mkdir %BUILDLOCATION%\Binaries\
+mkdir %BUILDLOCATION%\Modules\
+mkdir %BUILDLOCATION%\Images\
+mkdir %BUILDLOCATION%\Dependencies\
+copy ..\GLaDOSV3\bin\Release\net5.0\Binaries\     %BUILDLOCATION%\Binaries\
+copy ..\GLaDOSV3\bin\Release\net5.0\Modules\      %BUILDLOCATION%\Modules\
+copy ..\GLaDOSV3\bin\Release\net5.0\Images\       %BUILDLOCATION%\Images\
+copy ..\GLaDOSV3\bin\Release\net5.0\Dependencies\ %BUILDLOCATION%\Dependencies\
+IF %ERRORLEVEL% NEQ 0 GOTO err
+dotnet msbuild ..\GladosV3.sln /p:Configuration=Release "/p:Platform=x86"
+dotnet publish ..\GLaDOSV3\GladosV3.csproj -c Release -r portable -o .\Build\portable-x86
+
+set BUILDLOCATION=.\Build\portable-x86
+mkdir %BUILDLOCATION%\Binaries\
+mkdir %BUILDLOCATION%\Modules\
+mkdir %BUILDLOCATION%\Images\
+mkdir %BUILDLOCATION%\Dependencies\
+copy ..\GLaDOSV3\bin\Release\net5.0\Binaries\     %BUILDLOCATION%\Binaries\
+copy ..\GLaDOSV3\bin\Release\net5.0\Modules\      %BUILDLOCATION%\Modules\
+copy ..\GLaDOSV3\bin\Release\net5.0\Images\       %BUILDLOCATION%\Images\
+copy ..\GLaDOSV3\bin\Release\net5.0\Dependencies\ %BUILDLOCATION%\Dependencies\
+
+IF %ERRORLEVEL% NEQ 0 GOTO err
+
+del *.zip
+7z a release-portable.zip -mx9 -r .\Build\portable-* .\Build\portable-*
+7z l release-portable.zip
+7z a release-linux.zip -mx9 -r .\Build\linux-* .\Build\linux-*
+7z l release-linux.zip
+7z a release-windows.zip -mx9 -r .\Build\win-* .\Build\win-*
+7z l release-windows.zip
+7z a release-all.zip -mx9 *.zip
 del /q /f /s .\Build
 rmdir .\Build /s /q
 echo Build successful!
